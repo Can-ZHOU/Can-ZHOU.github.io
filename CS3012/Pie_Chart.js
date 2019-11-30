72797061850a17f776e2994eabc6cda1fe279d33
ZC <zhoucan1999@outlook.com>
2019-11-30 17:16:09 +0000

    CS3012

--- CS3012/Pie_Chart.js
+++ CS3012/Pie_Chart.js
@@ -1,5 +1,123 @@
 
 var chart = c3.generate({
+    bindto: '#chart_d',
+    data: {
+        json: json_data_forkCount,
+        type : 'pie',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    },
+    pie: {
+        label: {
+            format: function (value, ratio, id) {
+                return d3.format('')(value);
+            }
+        }
+    }
+});
+
+setTimeout(function () {
+    var chart = c3.generate({
+        bindto: '#chart_d',
+        data: {
+            json: json_data_diskUsage,
+            type : 'pie',
+            onclick: function (d, i) { console.log("onclick", d, i); },
+            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        pie: {
+            label: {
+                format: function (value, ratio, id) {
+                    return d3.format('')(value);
+                }
+            }
+        }
+    });
+}, 1500);
+
+setTimeout(function () {
+    var chart = c3.generate({
+        bindto: '#chart_d',
+        data: {
+            json: json_data_star,
+            type : 'pie',
+            onclick: function (d, i) { console.log("onclick", d, i); },
+            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        pie: {
+            label: {
+                format: function (value, ratio, id) {
+                    return d3.format('')(value);
+                }
+            }
+        }
+    });
+}, 3000);
+
+setTimeout(function () {
+    var chart = c3.generate({
+        bindto: '#chart_d',
+        data: {
+            json: json_data_forkCount,
+            type : 'donut',
+            onclick: function (d, i) { console.log("onclick", d, i); },
+            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        donut: {
+            label: {
+                format: function (value, ratio, id) {
+                    return d3.format('')(value);
+                }
+            }
+        }
+    });
+}, 4500);
+
+setTimeout(function () {
+    var chart = c3.generate({
+        bindto: '#chart_d',
+        data: {
+            json: json_data_diskUsage,
+            type : 'donut',
+            onclick: function (d, i) { console.log("onclick", d, i); },
+            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        donut: {
+            label: {
+                format: function (value, ratio, id) {
+                    return d3.format('')(value);
+                }
+            }
+        }
+    });
+}, 6000);
+
+setTimeout(function () {
+    var chart = c3.generate({
+        bindto: '#chart_d',
+        data: {
+            json: json_data_star,
+            type : 'donut',
+            onclick: function (d, i) { console.log("onclick", d, i); },
+            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        donut: {
+            label: {
+                format: function (value, ratio, id) {
+                    return d3.format('')(value);
+                }
+            }
+        }
+    });
+}, 7500);
+
+var chart = c3.generate({
     bindto: '#chart',
     data: {
         json: json_data_forkCount,
@@ -36,7 +154,6 @@ setTimeout(function () {
     });
 }, 3000);
 
-// Second time
 setTimeout(function () {
     var chart = c3.generate({
         bindto: '#chart',
